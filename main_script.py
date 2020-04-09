7aef75e9b554eb0810f5f23e9984dcced3a4ff0b
Ziyang Lan <ziyanglan0772@gmail.com>
2020-04-09 11:29:16 -0400

    Update main_script.py

--- main_script.py
+++ main_script.py
@@ -45,9 +45,10 @@ class Player:
         Returns:
             match_id(str): the match ID of the player.
         '''
+        match_id=''
         return match_id
     
-    #Unfinished
+    #Unfinished function
     def match_history(self,match_id):
         '''Finding the match history of the player.
        
@@ -64,16 +65,55 @@ class Player:
         match_history={}
         return match_history
 
-def player_winrate(user_id,game_index=10):
+def player_winrate(user_id,game_range=10):
+    '''Finding the player winrate within the newest matches.
+       
+    Args:
+    user_id(str):The summoners name of the player.
+    game_range(int): It indicates how many games to be checked on the player;s match history.
+        
+    Returns:
+    player_winrate(float): The winrate of the player.
+    '''
     return player_winrate
 
 def champ_winrate(champ_name,user_id):
+    '''Finding the winrate of the champion that the player is playing.
+       
+    Args:
+    champ_name(str):The name of the champion that is being played.
+    user_id(str): The summoners name of the player.
+        
+    Returns:
+    champ_winrate(float): The winrate of the champion.
+    '''
     return champ_winrate
 
 def against_winrate(champ_name1,champ_name2,user_id1,user_id2):
+    '''Finding the match wirnate of champions of two players.
+       
+    Args:
+    champ_name1(str):The name of the champion that is being played by the first player.
+    champ_name2(str):The name of the champion that is being played by the second player.
+    user_id1(str): The summoners name of the first player.
+    user_id2(str): The summoners name of the second player.
+    
+    Returns:
+    against_winrate(float): The winrate of this champion match.
+    '''
     return against_winrate
 
-def find_dodge(champ_winrate,against_winrate):
+def find_dodge(champ_winrate,against_winrate,player_winrate):
+    '''Determining whether the player need to dodge the game.
+       
+    Args:
+    champ_winrate(float): The winrate of the champion.
+    against_winrate(float): The winrate of this champion match.
+    player_winrate(float): The winrate of the player.
+    
+    Returns:
+    find_dodge(bool):Return true, if the player need to dodge the game, otherwise return false
+    '''
     return find_dodge
 
 def main():
